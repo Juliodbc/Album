7828a5a4806eade6e0ff84c0da2619ff876e9cfa
Juliodbc <deborbajulio@gmail.com>
2026-08-04 15:35:35 -0300

    android

--- tests/unit/example.spec.ts
+++ tests/unit/example.spec.ts
@@ -1,10 +1,10 @@
 import { mount } from '@vue/test-utils'
-import HomePage from '@/views/HomePage.vue'
+import AlbumPage from '@/views/AlbumPage.vue'
 import { describe, expect, test } from 'vitest'
 
-describe('HomePage.vue', () => {
-  test('renders home vue', () => {
-    const wrapper = mount(HomePage)
-    expect(wrapper.text()).toMatch('Ready to create an app?')
+describe('AlbumPage.vue', () => {
+  test('renders album title', () => {
+    const wrapper = mount(AlbumPage)
+    expect(wrapper.text()).toMatch('Meu Álbum')
   })
 })
